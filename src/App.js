8e615b9b5eebd08ce4509f7e3cdbe01dc4e15232
JackGre <gl.meh.bmes.od@gmail.com>
2021-08-12 17:53:31 +0300

    готово

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import React, {Component} from "react";
+import {useReducer} from "react";
 import CounterFeedback from "./CounterFeedback";
 
 
-
-class App extends Component {
-  state = {
+const countInitialState = {
+  
     good: 0,
     neutral: 0,
     bad: 0
-  }
+  
+};
+ 
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'good':
+      return { ...state, good: state.good + action.payload };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + action.payload };
+        case 'bad':
+      return { ...state, bad: state.bad + action.payload };
+    default:
+      return;
+    }
 
-  handleClick = (key) => {
-    this.setState((state) => ({
-      [key]: state[key] + 1,
-    }));
   };
-  
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-  countPositiveFeedbackPercentage = (total) => {
-    return Math.round((Number(this.state.good) / Number(total)) * 100);
-  }
-  
-  
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage(total);
-    const options = Object.keys(this.state);
 
-    return(
-      < CounterFeedback 
-        goodFeedback={good}
-        neutralFeedback={neutral}
-        badFeedback={bad}
+export default function App() {
+  const [state, dispatch] = useReducer(countReducer, countInitialState);
+
+  const total = state.good + state.neutral + state.bad;
+  const percentage = Math.round((Number(state.good) / Number(total)) * 100);
+  
+  const options = Object.keys(state)
+  
+
+  const handleClick = (option) => {
+     dispatch({type: option, payload: 1})
+  };
+
+
+  return (
+    < CounterFeedback 
+        goodFeedback={state.good}
+        neutralFeedback={state.neutral}
+        badFeedback={state.bad}
         total={total}
         percentage={percentage}
-        hendleClick={this.handleClick}
+        hendleClick={handleClick}
         options={options} 
       />
-    );
-  }
-}
+  )
+ }
 
-export default App;
